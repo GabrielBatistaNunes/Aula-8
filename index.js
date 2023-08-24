564ebfb23fe091a824fbeea146527216d7a36817
GabrielBatistaNunes <142436040+GabrielBatistaNunes@users.noreply.github.com>
2023-08-24 16:28:13 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,80 @@
+// this is a js file
+// Gabriel Batista Nunes
+
+/*
+1.
+a. false
+b. false
+c. true
+d. boolean
+
+2.
+vai dar os dois números juntos, por serem interpretados como texto
+
+3.
+let primeiroNumero = Number(prompt("Digite um numero!"));
+let segundoNumero = Number(prompt("Digite outro numero!"));
+const soma = primeiroNumero + segundoNumero;
+console.log(soma);
+
+B.
+
+1.
+
+//a
+age = Number(prompt("Sua idade"));
+console.log(age);
+
+//b
+bffage = Number(prompt("Idade do seu(a) melhor amigo(a)"));
+console.log(bffage);
+
+//c
+older = age > bffage;
+console.log("sua idade é maior que a do(a) seu(a) amigo(a)?", older);
+
+//d
+diferença = (Number(age - bffage));
+console.log ("Voce tem", diferença, "anos de diferença do(a) seu(a) amigo(a)");
+*/
+/*
+//2.
+
+//a
+npar = Number(prompt("insira um n° par"));
+console.log(npar);
+//b
+let rdp2 = npar % 2;
+console.log(rdp2);
+//c
+// Sim, o reultado de todo número par é 0
+
+//d
+// Quando ímpar o resultado sempre é 1
+*/
+
+//3.
+//a
+/*
+age = Number(prompt("Sua idade"));
+console.log("Idade em anos", age);
+meses = Number(age * 12);
+console.log("Sua idade em meses:", meses);
+days = Number(meses * 365);
+console.log("Sua idade em dias", days);
+hrs = Number(days * 24);
+console.log("Sua idade em horas", hrs);
+*/
+/*
+n1 = prompt("escolha um número");
+n2 = prompt("escolha outro número");
+console.log(Number(n1));
+console.log(Number(n2));
+console.log("O", n1, "é maior que", n2, "?", n1 > n2);
+console.log("O", n1, "é igual ao", n2, "?", n1 === n2);
+divd12 = Number(n1 % n2);
+divd21 = Number(n2 % n1);
+n3 = Number(0);
+console.log("O", n1, "é divisível por", n2, "?", divd12 <= n3);
+console.log("O", n2, "é divisível por", n1, "?", divd21 <= n3);
+*/
